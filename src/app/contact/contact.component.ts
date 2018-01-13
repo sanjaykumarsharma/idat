c92ae1cac31e0f0415b336cce15fc03bd7b88d1b
sanjay <saanjaaykumaar@gmail.com>
2018-01-13 08:59:40 +0530

    contact for working, next commit after upgrading angular to 5.2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Http } from '@angular/http';
+import { Resolve } from '@angular/router';
+import { Observable } from 'rxjs/RX';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-contact',
@@ -9,6 +13,8 @@ import { Http } from '@angular/http';
 })
 export class ContactComponent implements OnInit {
   apiEndpoint: 'http://localhost/idatum_api/api';
+  message_sent = false;
+  email_subject = '';
 
   @ViewChild('f') contactForm: NgForm;
 
@@ -18,11 +24,33 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    const value = form.value;
-    console.log(value);
+    const message = form.value;
+    console.log(message);
     const url = 'http://sanjyconsultinggroup.com/idatum_api/api' + '/mail' ;
     console.log(url);
-    this.http.post(url, value).subscribe(res => console.log(res.json()));
+
+      this.sendMail(message).subscribe(res => {
+        // console.log('AppComponent Success', res);
+        this.message_sent = true;
+        this.email_subject = message.subject;
+        console.log(message.subject);
+        console.log(this.email_subject);
+        form.reset();
+      }, error => {
+        console.log('AppComponent Error', error);
+      });
   }
+
+sendMail(message: any): Observable <any> {
+  return this.http.post('http://sanjyconsultinggroup.com/idatum_api/api/mail', message)
+    .map(response => {
+      console.log('Sending email was successfull', response);
+      return response;
+    })
+    .catch(error => {
+      console.log('Sending email got error', error);
+      return Observable.throw(error);
+    });
+}
 
 }
